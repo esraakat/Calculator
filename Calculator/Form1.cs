1d73c08ee0080cac1dcf1618f350ade7108b3260
esra akat <383196@ogr.ktu.edu.tr>
2021-07-25 19:20:40 +0300

    Add project files.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1, number2, add, product;
+
+            number1 = Convert.ToInt16(textBox1.Text);
+            number2 = Convert.ToInt16(textBox2.Text);
+            add = number1 + number2;
+            product = number1 * number2;
+
+            label4.Text = Convert.ToString(add);
+            label6.Text = Convert.ToString(product);
+        }
+    }
+}
